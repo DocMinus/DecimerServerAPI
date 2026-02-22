2d5205fd1c89851b594d2a851590db5070eb5e20
AlexMacM4 <alexander.minidis@gmail.com>
2026-02-22 11:44:15 +0100

    major install cleanup

--- win_make.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set IMAGE_NAME=decimer_api
-
-echo.
-echo ------------------------------------
-echo Starting the Docker build:
-docker build -t %IMAGE_NAME% .
-echo ------------------------------------
-echo Use 'docker-compose up -d' to start the container
